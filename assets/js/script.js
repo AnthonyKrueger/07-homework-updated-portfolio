9bc7837a9a2a6aa4aa8b66cb4c409da602eb23d4
aik <anthonykrueger0@gmail.com>
2021-05-31 21:48:27 -0500

    Made carousels sliders, finished last 2 sections, added mobile designs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
-  $(document).ready(function(){
-    $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
   });
+
